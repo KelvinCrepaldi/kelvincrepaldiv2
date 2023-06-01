9d4076b2f02b8702b2e9a35eca2ae30e2ce4a45b
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-01 20:35:08 -0300

    fix: react syntax highlight importation esm to csj

--- src/pages/notes/reactsyntaxhighlighter.tsx
+++ src/pages/notes/reactsyntaxhighlighter.tsx
@@ -6,13 +6,35 @@ import {
   okaidia,
   a11yDark,
   cb,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
+} from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function ReactSyntaxHighlighter() {
   const code = `
     function helloWorld() {
         console.log("Hello, World!");
     }`;
+
+  const code2 = `
+    import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+    // em Next trocar o caminho do estilo /esm por /cjs, se não, vai ocorrer um bug
+    import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+                                            
+    
+    function MyComponent() {
+      const code = \`function helloWorld() {
+        console.log("Hello, World!");
+      }\`;
+    
+      return (
+        <SyntaxHighlighter language="javascript" style={solarizedlight}>
+          {code}
+        </SyntaxHighlighter>
+      );
+    }
+    
+    export default MyComponent;
+    `;
   return (
     <>
       <div className="flex justify-start ">
@@ -22,7 +44,7 @@ export default function ReactSyntaxHighlighter() {
       </div>
       <div className="space-y-3">
         <h1 className="text-green-400">
-          Estilizando código com ReactSyntaxHighlighter
+          Estilizando código com React Syntax Highlighter
         </h1>
         <p>
           React Syntax Highlighter é uma biblioteca popular que exibe códigos de
@@ -58,24 +80,7 @@ export default function ReactSyntaxHighlighter() {
           desejada
         </p>
         <SyntaxHighlighter language="javascript" style={dracula}>
-          {`
-    import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-    import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-    
-    function MyComponent() {
-      const code = \`function helloWorld() {
-        console.log("Hello, World!");
-      }\`;
-    
-      return (
-        <SyntaxHighlighter language="javascript" style={solarizedlight}>
-          {code}
-        </SyntaxHighlighter>
-      );
-    }
-    
-    export default MyComponent;
-    `}
+          {code2}
         </SyntaxHighlighter>
         <p>
           Lembrando que para tratar caracteres especiais dentro dos blocos de
@@ -87,27 +92,27 @@ export default function ReactSyntaxHighlighter() {
         <h2 className="text-green-400">Styles</h2>
         <p>
           Existem dois caminhos para escolher estilos, sendo eles
-          react-syntax-highlighter/dist/esm/styles/hljs para HLJS e
-          react-syntax-highlighter/dist/esm/styles/prism para PRISM
+          react-syntax-highlighter/dist/cjs/styles/hljs para HLJS e
+          react-syntax-highlighter/dist/cjs/styles/prism para PRISM
         </p>
         <h2 className="text-green-400">Exemplos de styles:</h2>
-        <p>{`import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";`}</p>
+        <p>{`import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";`}</p>
         <SyntaxHighlighter language="javascript" style={dracula}>
           {code}
         </SyntaxHighlighter>
-        <p>{`import { coldarkCold } from "react-syntax-highlighter/dist/esm/styles/prism";`}</p>
+        <p>{`import { coldarkCold } from "react-syntax-highlighter/dist/cjs/styles/prism";`}</p>
         <SyntaxHighlighter language="javascript" style={coldarkCold}>
           {code}
         </SyntaxHighlighter>
-        <p>{`import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";`}</p>
+        <p>{`import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism";`}</p>
         <SyntaxHighlighter language="javascript" style={okaidia}>
           {code}
         </SyntaxHighlighter>
-        <p>{`import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";`}</p>
+        <p>{`import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";`}</p>
         <SyntaxHighlighter language="javascript" style={a11yDark}>
           {code}
         </SyntaxHighlighter>
-        <p>{`import { cb } from "react-syntax-highlighter/dist/esm/styles/prism";`}</p>
+        <p>{`import { cb } from "react-syntax-highlighter/dist/cjs/styles/prism";`}</p>
         <SyntaxHighlighter language="javascript" style={cb}>
           {code}
         </SyntaxHighlighter>
@@ -117,6 +122,24 @@ export default function ReactSyntaxHighlighter() {
         <p>{` Exemplo: <SyntaxHighlighter language="javascript" style={cb} showLineNumbers> {code} </SyntaxHighlighter>`}</p>
         <SyntaxHighlighter language="javascript" style={cb} showLineNumbers>
           {code}
+        </SyntaxHighlighter>
+        <h2 className="text-red-400">Possivel erro:</h2>
+        <p>
+          Em NextJS pode occorrer um erro de importação de estilo em
+          {`"`}react-syntax-highlighter/dist/esm/styles/prism{`"`}, uma possivel
+          solução é trocar o modulo esm para cjs ficando : {`"`}
+          react-syntax-highlighter/dist/cjs/styles/prism{`"`}
+        </p>
+        <SyntaxHighlighter language="javascript" style={cb} showLineNumbers>
+          {`
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {
+  dracula,
+  coldarkCold,
+  okaidia,
+  a11yDark,
+  cb,
+} from "react-syntax-highlighter/dist/cjs/styles/prism";`}
         </SyntaxHighlighter>
       </div>
     </>
