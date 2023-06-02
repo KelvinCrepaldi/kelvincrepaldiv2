3aa263372a90dddf5661a790067a85c95190cbfc
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-01 22:18:40 -0300

    fix: change esm to cjs import on cssadvanced.tsx note

--- src/pages/notes/cssadvanced.tsx
+++ src/pages/notes/cssadvanced.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 export default function TypescriptExpress() {
   const scrollbarText = `
   ::-webkit-scrollbar {
